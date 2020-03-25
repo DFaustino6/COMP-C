0741165b16009db9f185997becf13b159436501d
DFaustino6 <a61171@ualg.pt>
2020-03-25 20:05:35 +0000

    start

---
+++ src/Client.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Client
+ */
+public class Client {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        
+        sc.close();
+    }
+    
+}
